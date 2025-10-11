800c0b6ec1e69067246d67777ffdb73834eee582
sus <iamsuchabozo12345@gmail.com>
2025-10-11 13:36:12 -0700

    reorder config variables so grouped more logically

--- shared/config.ts
+++ shared/config.ts
@@ -1,12 +1,11 @@
 export const Config = {
-	playerRadius: 15,
-	dashDistance: 300,
-	headLength: 30,
-
-	scale: 2,
-
-	dashCooldown: 0.5, // in seconds
-
 	canvasWidth: 900,
 	canvasHeight: 600,
+	scale: 2,
+
+	playerRadius: 15,
+	headLength: 30,
+
+	dashDistance: 300,
+	dashCooldown: 0.5, // in seconds
 }
