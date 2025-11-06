9c1d0392d5ee430a75dcc9d24d13db7d7e8a01b4
sus <iamsuchabozo12345@gmail.com>
2025-11-05 20:51:59 -0800

    move update player into player class

--- shared/player.ts
+++ shared/player.ts
@@ -1,5 +1,7 @@
 import { config } from "./config";
+import { clampPos } from "./math";
 import { MoveData } from "./moveData";
+import { PlayerSegment } from "./state";
 import { v2, Vec2 } from "./v2";
 
 export class Player {
@@ -74,5 +76,70 @@ export class Player {
       this.dashing = move.dashing;
       this.dashProgress = move.dashProgress;
       this.dashVel = move.dashVel;
+   }
+
+   update(dt: number): PlayerSegment[] {
+      this.dashProgress = Math.min(this.dashProgress + dt, config.dashCooldown);
+      
+      let vel = this.moveVel;
+
+      if (this.dashing) {
+         vel = this.dashVel;
+
+         // Check if dash should end during this frame
+         const dashTimeRemaining = config.dashDuration - (this.dashProgress - dt);
+
+         if (dashTimeRemaining <= 0) {
+            this.dashing = false;
+            vel = this.moveVel;
+         } else if (dashTimeRemaining < dt) {
+            const dashPortion = dashTimeRemaining;
+            const movePortion = dt - dashTimeRemaining;
+
+            const dashMovement = v2.mul(this.dashVel, dashPortion);
+            const normalMovement = v2.mul(this.moveVel, movePortion);
+
+            let segments: PlayerSegment[] = [];
+
+            segments.push({
+               player: this,
+               startPos: this.pos,
+               velocity: this.dashVel,
+               dashing: true,
+               startTime: 0,
+               endTime: dashTimeRemaining,
+            });
+
+            this.pos = clampPos(v2.add(this.pos, dashMovement));
+
+            segments.push({
+               player: this,
+               startPos: this.pos,
+               velocity: this.moveVel,
+               dashing: false,
+               startTime: dashTimeRemaining,
+               endTime: dt,
+            });
+
+            this.pos = clampPos(v2.add(this.pos, normalMovement));
+            this.dashing = false;
+
+            return segments;
+         }
+      }
+
+      const startPos: Vec2 = this.pos;
+      this.pos = clampPos(v2.add(this.pos, v2.mul(vel, dt)));
+
+      return [
+         {
+            player: this,
+            startPos: startPos,
+            velocity: vel,
+            dashing: this.dashing,
+            startTime: 0,
+            endTime: dt,
+         },
+      ];
    }
 }
