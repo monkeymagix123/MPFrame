06f6bb1bd269932792030097ead128018bbf3be7
sus <iamsuchabozo12345@gmail.com>
2025-12-07 13:36:50 -0800

    add dash invulnerable to player stats

--- shared/playerStats.ts
+++ shared/playerStats.ts
@@ -2,14 +2,22 @@ import { config } from "./config";
 
 // does not need serializing because constructor auto-creates it
 export class PlayerStats {
+    // Dash-related stats
+    // Movement
     dashSpeed: number = config.dashSpeed;
     dashDistance: number = config.dashDistance;
     dashCooldown: number = config.dashCooldown;
 
+    // Effects
+    dashInvulnerable: boolean = false;
+
+    // Damage
     damage: number = config.dashDamage;
 
+    // Health-related stats
     maxHealth: number = config.maxHealth;
 
+    // Movement stats
     moveSpeed: number = config.moveSpeed;
 
     constructor() {}
