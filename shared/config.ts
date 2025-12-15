2a017643bdc4dbc8b52d731d83c0846763dec17d
sus <iamsuchabozo12345@gmail.com>
2025-12-15 13:22:07 -0800

    debuff upgrades

--- shared/config.ts
+++ shared/config.ts
@@ -10,10 +10,10 @@ export const config = {
    playerLength: 30,
    headLength: 30,
 
-   moveSpeed: 180,
+   moveSpeed: 80,
 
-   dashCooldown: 2.5,
-   dashDistance: 250,
+   dashCooldown: 5.0,
+   dashDistance: 100,
    get dashDuration() { // includes part of cooldown, use for computed property
       return this.dashDistance / (this.moveSpeed * this.dashSpeedMultiplier);
    },
